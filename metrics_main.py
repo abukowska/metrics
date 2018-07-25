34e968a934fd78faaf0913ee8a72a3e2d4227c4c
abukowska <bukowska.joa@gmail.com>
2018-07-25 23:18:59 +0200

    preliminary commit for metrics

---
+++ metrics_main.py
@@ -0,0 +1,74 @@
+import csv
+import subprocess
+import time
+
+import numpy as np
+import schedule
+
+from supporting_methods import support
+
+START = time.time()
+PERIOD_OF_TIME_SEC = 20
+REPEATING_SCHEDULE_SEC = 6
+GRAPH_FILE_NAME = "graph.png"
+
+visits_per_minute = []
+line_to_start_read = 0
+
+def count_user_visits():
+    global line_to_start_read
+    global visits_per_minute
+    # TODO: try, catch
+    subprocess.call("(scp -i ~/.ssh/metrics ec2-user@34.242.250.224:~/logs/access.log .)", shell=True)
+    data = np.genfromtxt('access.log', dtype=str, delimiter='"', usecols=(0,2,5))
+    print("line to start " + str(line_to_start_read))
+    if line_to_start_read != 0:
+        data = data[line_to_start_read:]
+    else:
+        line_to_start_read = len(data.tolist())
+
+    users = []
+
+    for el in data:
+        user_agent = el[2].strip()
+        status_code = el[1].strip().split()[0]
+
+        if "googlebot" in user_agent.lower() or status_code.startswith("4"):
+            continue
+
+        ip_address = el[0].strip().split()[0]
+
+        one_unique_user = (ip_address, user_agent)
+        users.append(one_unique_user)
+
+    unique_users = list(set([tuple(t) for t in users]))
+    visits_per_minute.append(len(unique_users))
+    print("unique users:" + str(len(unique_users)))
+    print("------------")
+
+
+def main():
+    count_user_visits()
+    schedule.every(REPEATING_SCHEDULE_SEC).seconds.do(count_user_visits)
+
+    while True:
+        schedule.run_pending()
+        if time.time() > START + PERIOD_OF_TIME_SEC:
+            break
+
+    with open('output/metrics_results.csv', 'w') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+        spamwriter.writerow(('min', 'no of visits'))
+        for record in enumerate(visits_per_minute, 1):
+            spamwriter.writerow(record)
+
+    x_axis_time = list(x for x in range(1, len(visits_per_minute)+1))
+    y_axis_visits = visits_per_minute
+
+    support.draw_chart_and_save_to_picture_format(x_axis_time, y_axis_visits, PERIOD_OF_TIME_SEC, GRAPH_FILE_NAME)
+
+    support.prepare_and_build_HTML_file(GRAPH_FILE_NAME, "output/metrics_results.csv")
+
+
+if __name__ == "__main__":
+    main()
---
+++ supporting_methods/support.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def prepare_and_build_HTML_file(graph_file_name, csv_file):
+    df = pd.read_csv(csv_file)
+    summary_table = df.describe()
+    summary_table = summary_table.to_html().replace('<table border="1" class="dataframe">','<table class="table table-striped">')
+
+    html_string = '''
+    <html>
+        <head>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+            <style>body{margin:0 100; background:whitesmoke;}</style>
+        </head>
+        <body>
+            <h1>Server visits per each minute</h1>
+            <iframe width="1000" height="550" frameborder="0" seamless="seamless" \
+            scrolling="no" src="''' + graph_file_name + '''"></iframe>
+            <p>this is impressive</p>
+
+            <h3>Summary table:</h3>
+            ''' + summary_table + '''
+        </body>
+    </html>'''
+
+    with open('output/html_metric_results.html', 'w') as htmlfile:
+        htmlfile.write(html_string)
+
+def draw_chart_and_save_to_picture_format(x_axis_data, y_axis_data, period_of_time, graph_file_name):
+        fig = plt.figure(figsize=(10, 8))
+        plt.plot(x_axis_data, y_axis_data, 'go')
+        plt.xlabel('time [min]', fontsize=16)
+        plt.ylabel('number of visits', fontsize=16)
+        plt.grid(axis='y', linestyle='--')
+        plt.xticks(np.arange(0, period_of_time/60, 1.0))
+        fig.savefig("output/" + graph_file_name)
